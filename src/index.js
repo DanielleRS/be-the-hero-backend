f0e81a92459c5e600ea5d55b76f66fcd39564207
Danielle Rodrigues <daniellerodris@gmail.com>
2020-03-25 18:35:14 -0300

    Criação da base da aplicação

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+app.listen(3333);
